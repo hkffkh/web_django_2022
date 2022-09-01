9154b0c43c4ce9fafdefc08df49e3e2f961e39e5
hkffkh <825703939@qq.com>
2022-09-01 15:13:34 +0800

    update redis 设置

--- web_django_app/settings.py
+++ web_django_app/settings.py
@@ -121,6 +121,41 @@ DATABASES = {
 }
 
 
+# # 设置redis缓存
+# CACHES = {
+#     # 默认缓存
+#     "default": {
+#         "BACKEND": "django_redis.cache.RedisCache",
+#         # 项目上线时,需要调整这里的路径
+#         "LOCATION": "redis://127.0.0.1:6379/0", # 0号库
+#
+#         "OPTIONS": {
+#             "CLIENT_CLASS": "django_redis.client.DefaultClient",
+#         }
+#     },
+#     # 提供给xadmin或者admin的session存储
+#     "session": {
+#         "BACKEND": "django_redis.cache.RedisCache",
+#         "LOCATION": "redis://127.0.0.1:6379/1", # 1号库
+#         "OPTIONS": {
+#             "CLIENT_CLASS": "django_redis.client.DefaultClient",
+#         }
+#     },
+#     # 提供存储短信验证码
+#     "sms_code":{
+#         "BACKEND": "django_redis.cache.RedisCache",
+#         "LOCATION": "redis://127.0.0.1:6379/2",  # 2号库
+#         "OPTIONS": {
+#             "CLIENT_CLASS": "django_redis.client.DefaultClient",
+#         }
+#     }
+# }
+#
+# # 设置xadmin用户登录时,登录信息session保存到redis
+# SESSION_ENGINE = "django.contrib.sessions.backends.cache"
+# SESSION_CACHE_ALIAS = "session"
+
+
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
